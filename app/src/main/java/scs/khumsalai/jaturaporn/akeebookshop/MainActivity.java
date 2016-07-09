7d130ad8f5cae68218d662166d1bade87288c566
Jaturaporn <jaturaporn@gmail.com>
2016-07-09 10:15:14 +0700

    Create SignUp Completed

--- app/src/main/java/scs/khumsalai/jaturaporn/akeebookshop/MainActivity.java
+++ app/src/main/java/scs/khumsalai/jaturaporn/akeebookshop/MainActivity.java
@@ -1,13 +1,20 @@
 package scs.khumsalai.jaturaporn.akeebookshop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+        setContentView(R.layout.activity_main); //    ดึงหน้ากากมาครอบหน้า
+    }   // Main Method
+
+    //อันนี้ พิมพ์เอง
+    public void clickSignupMain(View view) {
+        startActivity(new Intent(MainActivity.this,SignUpActivity.class));  // เพื่อสั่งให้ เมื่อคลิกแล้วให้ Intent  ไปที่อีก Activity หนึ่ง ที่ชื่อ SignupActivity
+
+}   // Main Class
